cbb626d5410f5cb4f9e331efe1d9ab092f325c4a
oksanaduga <akakiikaramazov@gmail.com>
2021-04-01 22:44:39 +0300

    add ssl to db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const pgp = require('pg-promise')();
 
 const cn = process.env.DATABASE_URL;
-const db = pgp(cn);
+const db = pgp(cn + '?ssl=true');
 console.log('cn: ', cn);
 console.log('db: ', db);
 module.exports = db;
